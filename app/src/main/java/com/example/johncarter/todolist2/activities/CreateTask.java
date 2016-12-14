6372020559c41cfb73f887e8f07e019b43439d74
k-Francisco <piattosnovalays@gmail.com>
2016-12-15 01:11:38 +0800

    second commit

---
+++ app/src/main/java/com/example/johncarter/todolist2/activities/CreateTask.java
@@ -0,0 +1,28 @@
+package com.example.johncarter.todolist2.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.johncarter.todolist2.R;
+
+public class CreateTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_task);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+}
