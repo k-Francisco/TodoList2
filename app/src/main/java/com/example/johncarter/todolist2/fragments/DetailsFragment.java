879e0ebc3e2d9141796829d108154a8d8c0034bf
Anton Wycoco <Anton Wycoco>
2016-12-16 04:05:03 +0800

    Anton Commits

--- app/src/main/java/com/example/johncarter/todolist2/fragments/DetailsFragment.java
+++ app/src/main/java/com/example/johncarter/todolist2/fragments/DetailsFragment.java
@@ -1,46 +1,41 @@
 package com.example.johncarter.todolist2.fragments;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.johncarter.todolist2.R;
 import com.example.johncarter.todolist2.adapters.TodoRecycler;
 
 /**
- * A placeholder fragment containing a simple view.
+ * Created by dell on 12/16/2016.
  */
-public class TodoFragment extends Fragment {
 
-    public TodoFragment() {
+public class DetailsFragment extends Fragment{
+
+    public DetailsFragment(){
+
     }
-
 
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
     RecyclerView.Adapter adapter;
-    TextView des, date, time;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
-//        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
-//        layoutManager = new LinearLayoutManager(view.getContext());
-//        recyclerView.setLayoutManager(layoutManager);
-//
-//        adapter = new TodoRecycler();
-//        recyclerView.setAdapter(adapter);
-        des = (TextView)view.findViewById(R.id.txtDescription);
-        date = (TextView)view.findViewById(R.id.txtDate);
-        time = (TextView)view.findViewById(R.id.txtTime);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
+        layoutManager = new LinearLayoutManager(view.getContext());
+        recyclerView.setLayoutManager(layoutManager);
 
+        adapter = new TodoRecycler();
+        recyclerView.setAdapter(adapter);
         return view;
     }
 }
